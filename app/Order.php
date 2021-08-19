f20d0b29db6fa9f2987aa0f825a0526fee4fcbc5
Adeleke Hammed <lekhad19@gmail.com>
2021-08-19 01:16:13 +0100

    ShipRocket API Tutorial _ ShipRocket API Integration with Laravel _ Push Order via Curl with Laravel

--- app/Order.php
+++ app/Order.php
@@ -11,4 +11,83 @@ class Order extends Model
     public function orders_products(){
         return $this->hasMany('App\OrdersProduct', 'order_id');
     }
+
+    public function order_items(){
+        return $this->hasMany('App\OrdersProduct', 'order_id');
+    }
+
+    public static function pushOrder($order_id){
+        $orderDetails = Order::with('order_items')->where('id', $order_id)->first()->toArray();
+        $orderDetails['order_id'] =         $orderDetails['id'];
+        $orderDetails['order_date'] =       $orderDetails['created_at'];
+        $orderDetails['pickup_location'] =  "Test";
+        $orderDetails['channel_id'] =       "19909200";
+        $orderDetails['comment'] =          "Test10";
+        $orderDetails['billing_customer_name'] = $orderDetails['name'];
+        $orderDetails['billing_last_name'] = "";
+        $orderDetails['billing_address'] = "";
+        $orderDetails['billing_address_2'] = $orderDetails['address'];
+        $orderDetails['billing_city'] = $orderDetails['city'];
+        $orderDetails['billing_pincode'] = $orderDetails['pincode'];
+        $orderDetails['billing_state'] = $orderDetails['state'];
+        $orderDetails['billing_country'] = $orderDetails['country'];
+        $orderDetails['billing_email'] = $orderDetails['address'];
+        $orderDetails['billing_phone'] = $orderDetails['address'];
+        $orderDetails['shipping_is_billing'] = true;
+        $orderDetails['shipping_customer_name'] = $orderDetails['name'];
+        $orderDetails['shipping_last_name'] = "";
+        $orderDetails['shipping_address'] = $orderDetails['address'];
+        $orderDetails['shipping_address_2'] = "";
+        $orderDetails['shipping_city'] = $orderDetails['city'];
+        $orderDetails['shipping_pincode'] = $orderDetails['pincode'];
+        $orderDetails['shipping_state'] = $orderDetails['state'];
+        $orderDetails['shipping_country'] = $orderDetails['country'];
+        $orderDetails['shipping_email'] = $orderDetails['email'];
+        $orderDetails['shipping_phone'] = $orderDetails['mobile'];
+        foreach($orderDetails['order_items'] as $key => $item){
+            $orderDetails['order_items'][$key]['name'] = $item['product_name'];
+            $orderDetails['order_items'][$key]['sku'] = $item['product_code'];
+            $orderDetails['order_items'][$key]['units'] = $item['product_qty'];
+            $orderDetails['order_items'][$key]['selling_price'] = $item['product_price'];
+            $orderDetails['order_items'][$key]['discount'] = "";
+            $orderDetails['order_items'][$key]['tax'] = "";
+            $orderDetails['order_items'][$key]['hsn'] = "";
+        }
+        $orderDetails['shipping_charges'] = 0;
+        $orderDetails['giftwrap_charges'] = 0;
+        $orderDetails['transaction_charges'] = 0;
+        $orderDetails['total_discount'] = 0;
+        $orderDetails['sub_total'] = $orderDetails['grand_total'];
+        $orderDetails['length'] =   1;
+        $orderDetails['breadth'] =  1;
+        $orderDetails['height'] =   1;
+        $orderDetails['weight'] =   1;
+        // echo "<pre>"; print_r(json_encode($orderDetails)); die;
+        $orderDetails = json_encode($orderDetails);
+
+        // Generate Access Token
+        $c = curl_init();
+        $url = "https://apiv2.shiprocket.in/v1/external/auth/login";
+        curl_setopt($c, CURLOPT_URL, $url);
+        curl_setopt($c, CURLOPT_POST, 1);
+        curl_setopt($c, CURLOPT_POSTFIELDS, "email=prymable@gmail.com&password=Adeleke1992");
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        $server_output = curl_exec($c);
+        curl_close($c);
+        $server_output = json_decode($server_output, true);
+        // echo "<pre>"; print_r($server_output); die;
+
+        // Create Order in ShipRocket
+        $url = "https://apiv2.shiprocket.in/v1/external/orders/create/adhoc";
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_POST, 1);
+        curl_setopt($c, CURLOPT_POSTFIELDS, $orderDetails);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer '.$server_output['token'].''));
+        $result = curl_exec($c);
+        curl_close($c);
+        print_r($result); die;
+    }
 }
