1fecdcb6f374d59c789472fd45f4bb11c05e2bb3
Adeleke Hammed <lekhad19@gmail.com>
2021-09-07 00:26:43 +0100

    feat: rubbish error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,18 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(ProductsAttributesTableSeeder::class);
-        $this->call(ProductsImagesTableSeeder::class);
-        $this->call(BrandsTableSeeder::class);
-        $this->call(BannersTableSeeder::class);
-        $this->call(CouponsTableSeeder::class);
-        $this->call(DeliveryAddressTableSeeder::class);
-        $this->call(OrderStatusTableSeeder::class);
-        $this->call(CmsPagesTableSeeder::class);
+        // $this->call(SectionsTableSeeder::class);
+        // $this->call(CategoryTableSeeder::class);
+        // $this->call(ProductsTableSeeder::class);
+        // $this->call(ProductsAttributesTableSeeder::class);
+        // $this->call(ProductsImagesTableSeeder::class);
+        // $this->call(BrandsTableSeeder::class);
+        // $this->call(BannersTableSeeder::class);
+        // $this->call(CouponsTableSeeder::class);
+        // $this->call(DeliveryAddressTableSeeder::class);
+        // $this->call(OrderStatusTableSeeder::class);
+        // $this->call(CmsPagesTableSeeder::class);
     }
 }
