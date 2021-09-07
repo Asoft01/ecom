909533e110b045577c595f5cd80b0af68ec44025
Adeleke Hammed <lekhad19@gmail.com>
2021-09-07 08:23:25 +0100

    Minor fix

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -18,19 +18,19 @@ class AdminsTableSeeder extends Seeder
         DB::table('admins')->delete();
         $adminRecords = [
             [
-                'id'=>1, 'name'=> 'admin', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'lekhad19@gmail.com', 'password'=> '$2y$10$J/3Tbs8.GxTFjQq0kXBY1OkVNFjKrx9JHkP8IHHFUMcFQcrQHlVjy', 'image'=> '', 'status'=> 1
+                'id'=>1, 'name'=> 'admin', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'lekhad19@gmail.com', 'password'=> '$2y$10$yDGUizcHMb/TwHAVDQLR9.N0eiQA5nv3HuZAA52koQ3BXCLSLfqA2', 'image'=> '', 'status'=> 1
             ], 
             [
-                'id'=>2, 'name'=> 'john', 'type'=> 'subadmin', 'mobile'=> '0830909099', 'email'=> 'john@gmail.com', 'password'=> '$2y$10$J/3Tbs8.GxTFjQq0kXBY1OkVNFjKrx9JHkP8IHHFUMcFQcrQHlVjy', 'image'=> '', 'status'=> 1
+                'id'=>2, 'name'=> 'john', 'type'=> 'subadmin', 'mobile'=> '0830909099', 'email'=> 'john@gmail.com', 'password'=> '$$2y$10$yDGUizcHMb/TwHAVDQLR9.N0eiQA5nv3HuZAA52koQ3BXCLSLfqA2', 'image'=> '', 'status'=> 1
             ],
             [
-                'id'=>3, 'name'=> 'steve', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'steve@gmail.com', 'password'=> '$2y$10$J/3Tbs8.GxTFjQq0kXBY1OkVNFjKrx9JHkP8IHHFUMcFQcrQHlVjy', 'image'=> '', 'status'=> 1
+                'id'=>3, 'name'=> 'steve', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'steve@gmail.com', 'password'=> '$$2y$10$yDGUizcHMb/TwHAVDQLR9.N0eiQA5nv3HuZAA52koQ3BXCLSLfqA2', 'image'=> '', 'status'=> 1
             ],
             [
-                'id'=>4, 'name'=> 'amit', 'type'=> 'subadmin', 'mobile'=> '0830909099', 'email'=> 'amit@gmail.com', 'password'=> '$2y$10$J/3Tbs8.GxTFjQq0kXBY1OkVNFjKrx9JHkP8IHHFUMcFQcrQHlVjy', 'image'=> '', 'status'=> 1
+                'id'=>4, 'name'=> 'amit', 'type'=> 'subadmin', 'mobile'=> '0830909099', 'email'=> 'amit@gmail.com', 'password'=> '$$2y$10$yDGUizcHMb/TwHAVDQLR9.N0eiQA5nv3HuZAA52koQ3BXCLSLfqA2', 'image'=> '', 'status'=> 1
             ],
             [
-                'id'=>5, 'name'=> 'mary', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'mary@gmail.com', 'password'=> '$2y$10$J/3Tbs8.GxTFjQq0kXBY1OkVNFjKrx9JHkP8IHHFUMcFQcrQHlVjy', 'image'=> '', 'status'=> 1
+                'id'=>5, 'name'=> 'mary', 'type'=> 'admin', 'mobile'=> '0830909099', 'email'=> 'mary@gmail.com', 'password'=> '$$2y$10$yDGUizcHMb/TwHAVDQLR9.N0eiQA5nv3HuZAA52koQ3BXCLSLfqA2', 'image'=> '', 'status'=> 1
             ],
             
         ];
